8da17e04a67a5961eba3e659085d2a77ce761c0d
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-02-17 14:51:39 +0400

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # app/__init__.py
 import os
-from flask import Flask, g, abort
+from flask import Flask, g, abort, request
 from app.extensions import db_session, DBModel
 from app.translation import get_pagetext
 
@@ -19,10 +19,15 @@ def create_app():
     # 请求前确认翻译文件正确读取
     @app.before_request
     def before_request():
-        BASE_DIR = os.path.join(os.getcwd(), 'app', 'base')
-        g.PageText = get_pagetext(BASE_DIR, 'locale')
+        bp_name = request.blueprint.removeprefix('base.')
+        locale_names = ['locale']
+        if set([bp_name]).issubset(set(Config.LOCALES)):
+            locale_names = locale_names + [bp_name]
+        print(f'bp_name: {bp_name}, locale_names: {locale_names}, Config.LOCALES: {Config.LOCALES}\n')
+        g.PageText = get_pagetext(locale_names)
         if not g.PageText:
-        # 如果翻译文件读取失败，返回错误信息    
+            # 如果翻译文件读取失败，返回错误信息
+            print(f'Fail to load pagetext. {locale_names}')
             abort(404)
 
     return app
