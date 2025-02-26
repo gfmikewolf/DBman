e3f7fb9b69e616573d3f0c34bbddac973f1cdcd6
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-02-26 09:33:20 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@
 from flask import Flask, abort, session
 from app.translation import get_pagetext
 
-
-
 def create_app():
     app = Flask(__name__)
     
@@ -15,12 +13,15 @@ def create_app():
     app.register_blueprint(base_bp)
 
     # 请求前确认翻译文件正确读取
-    PageText = get_pagetext(Config.LOCALES)
-    if not PageText:
-        # 如果翻译文件读取失败，返回错误信息
-        print(f'Fail to load pagetext. {Config.LOCALES}')
-        abort(404)
-    else:
-        session['PageText'] = PageText
+    @app.before_request
+    def before_request():
+        PageText = get_pagetext(Config.LOCALES)
+        if not PageText:
+            # 如果翻译文件读取失败，返回错误信息
+            print(f'Fail to load pagetext. {Config.LOCALES}')
+            abort(404)
+        else:
+            # 将字典注册到进程变量
+            session['PageText'] = PageText
 
     return app
