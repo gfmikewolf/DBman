146783b0902bbcfc1fcd2211b9512d81f3d7a393
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-07 17:16:18 +0400

    Update __init__.py

--- app/base/__init__.py
+++ app/base/__init__.py
@@ -1,7 +1,7 @@
-# /app/base/__init__.py
+# app/base/__init__.py
 from flask import Blueprint
 from app.translation import set_lang
-from .admin import admin_bp
+from .crud import crud_bp
 from .views import index
 
 base_bp = Blueprint('base', __name__, template_folder='templates', static_folder='static', static_url_path='/base/static')
@@ -9,4 +9,4 @@ base_bp = Blueprint('base', __name__, template_folder='templates', static_folder
 base_bp.route('/')(index)
 base_bp.route('/set_lang/<lang>')(set_lang)
 
-base_bp.register_blueprint(admin_bp)
+base_bp.register_blueprint(crud_bp)
