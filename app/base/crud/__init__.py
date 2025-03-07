707c8cfd3da09060144a93da58243918687ee7c1
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-07 17:19:48 +0400

    Create __init__.py

---
+++ app/base/crud/__init__.py
@@ -0,0 +1,10 @@
+# app/base/crud/__init__.py
+from flask import Blueprint
+from .views import index, view_table, modify_record, delete_record
+
+crud_bp = Blueprint('crud', __name__, template_folder='templates', url_prefix='/crud')
+
+crud_bp.route('/view_table/<table_name>', methods=['GET', 'POST'])(view_table)
+crud_bp.route('/modify_record/<table_name>/<record_id>', methods=['GET', 'POST'])(modify_record)
+crud_bp.route('/delete_record/<table_name>/<record_id>', methods=['POST'])(delete_record)
+crud_bp.route('/', methods=['GET'])(index)
