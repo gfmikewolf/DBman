17d83a52243940646adf7f112754ad2a93b3e3ff
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-17 09:43:14 +0400

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,10 +1,2 @@
 # app/database/models.py
-from .contract import Base as DB_Contract, DataJson as DJ_Contract
-
-DB_map = {
-    'contract': DB_Contract
-}
-
-DJ_map = {
-    'contract': DJ_Contract
-}
+from .contract import Base, DataJson
