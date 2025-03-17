668d50f7e770557ef22ac4ee584b82ce74483c61
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-17 09:28:35 +0400

    Update __init__.py

--- app/database/contract/__init__.py
+++ app/database/contract/__init__.py
@@ -1,2 +1,32 @@
-from .dbmodels import Contract, Amendment, Clause, Entity, Entitygroup
-from .clauses import ClauseExpiry, ClauseEntity, ClauseScope
+from ..datajson import DataJson
+from ..base import Base
+
+# database models
+from .dbmodels import Contract
+from .dbmodels import Amendment
+from .dbmodels import Clause
+from .dbmodels import Entity
+from .dbmodels import Entitygroup
+
+# DataJson models
+from .clauses import ClauseEntity
+from .clauses import ClauseExpiry
+from .clauses import ClauseScope
+
+# type Enums
+from .clausetypes import ClauseAction, ClausePos, ClauseType
+
+Base.model_map = {
+    'contract': Contract,
+    'amendment': Amendment,
+    'clause': Clause,
+    'entity': Entity,
+    'entitygroup': Entitygroup
+}
+
+DataJson.class_map = {
+    'data_json': DataJson,
+    'clause_expiry': ClauseExpiry,
+    'clause_entity': ClauseEntity,
+    'clause_scope': ClauseScope
+}
