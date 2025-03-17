d2fbefc432435bb0af11b245ff7ab839f7efdcaf
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-17 11:34:29 +0400

    Update views.py

--- app/base/views.py
+++ app/base/views.py
@@ -1,6 +1,6 @@
 # app/base/views.py
 from flask import render_template
-from app.utils import PageNavigation
+from app.utils.templates import PageNavigation
 
 # 本蓝图的基础导航
 navigation = PageNavigation ({
