ec71dad53b222de37b1e1900f2b6d8ed6042277c
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-20 16:05:26 +0400

    Update scripts.js

--- app/base/static/js/scripts.js
+++ app/base/static/js/scripts.js
@@ -8,17 +8,37 @@ import { ModalAlert } from './modal-dbman.js';
 import { Datatable } from './datatable-dbman.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // 初始化所有的工具提示
+    // init bs tooltips
     const tooltipList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     if (tooltipList.length > 0) {
         initializeTooltips(tooltipList);
     }
-    const alertModal = new ModalAlert('#modal-alert');
-    if(!alertModal.active)
-        console.log('Failed to initialize alertModal.');
-    const datatable = new Datatable('#datatable-table-main', alertModal);
-    if(!datatable.active)
-        console.log('Failed to initialize datatable.');
+    // init modules on demand
+    const metaModules = document.querySelector('meta[name="modules"]');
+    
+    if (metaModules) {
+        const strModules = metaModules.content;
+        if (strModules) {
+            const moduleNames = strModules.split(',');
+            let alertModal;
+            if (moduleNames.includes('modal-alert')) {
+                alertModal = new ModalAlert('#modal-alert');
+                if(!alertModal.active) {
+                    console.log('Failed to initialize ModalAlert instance.');
+                }
+            }
+            let datatable;
+            if (moduleNames.includes('datatable')) {
+                datatable = new Datatable('#datatable-table-main', alertModal);
+                if(!datatable.active) {
+                    console.log('Failed to initialize Datatable instance.');
+                }
+            } 
+            if (moduleNames.includes('form-modify')) {
+
+            }
+        }
+    }
 });
 
 function initializeTooltips(tooltipList) {
