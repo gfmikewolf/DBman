3c417e5aa0367faee11e34a48253648352f269bd
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-25 11:53:17 +0400

    Update __init__.py

--- app/base/crud/__init__.py
+++ app/base/crud/__init__.py
@@ -1,6 +1,7 @@
 # app/base/crud/__init__.py
 from flask import Blueprint
-from .views import index, view_table, modify_record, delete_record, view_record, get_datajson
+from .views import index, view_table, modify_record, delete_record, view_record
+from app.base.api import api_bp
 
 crud_bp = Blueprint('crud', __name__, template_folder='templates', url_prefix='/crud')
 
@@ -9,4 +10,3 @@ crud_bp.route('/modify_record/<table_name>/<pks>', methods=['GET', 'POST'])(modi
 crud_bp.route('/delete_record/<table_name>/<pks>', methods=['DELETE'])(delete_record)
 crud_bp.route('/', methods=['GET'])(index)
 crud_bp.route('/view_record/<table_name>/<pks>', methods=['GET'])(view_record)
-crud_bp.route('/get_datajson/<datajson_id>', methods=['GET'])(get_datajson)
