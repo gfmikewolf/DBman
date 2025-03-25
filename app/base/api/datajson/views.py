c53ee7d5acb629364d924276cc63936b7293369f
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-25 15:39:16 +0400

    Update views.py

--- app/base/api/datajson/views.py
+++ app/base/api/datajson/views.py
@@ -1,10 +1,10 @@
-from typing import Any
+from flask import jsonify
+from app.database.base import serialize_value
 from app.extensions import DataJson
 
-def get_datajson_structure(datajson_id: str) -> dict[str, Any]:
-    
+def get_datajson_structure(datajson_id: str):
     datajson_cls = DataJson.class_map.get(datajson_id, None)
     if not datajson_cls:
         raise ValueError(f'Invalid datajson_id: {datajson_id}')
-    datajson_construct = datajson_cls.get_structure()
-    return datajson_construct
+    structure = serialize_value(datajson_cls.get_structure())
+    return jsonify(success=True, data=structure)
