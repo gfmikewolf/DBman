9616b128b1b99439aeb0af8eb6f38f87663a29ee
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-03-25 15:39:49 +0400

    Update __init__.py

--- app/base/api/datajson/__init__.py
+++ app/base/api/datajson/__init__.py
@@ -6,5 +6,5 @@ from .views import get_datajson_structure
 datajson_bp = Blueprint('datajson', __name__, url_prefix='/datajson')
 
 datajson_bp.route(
-    '/datajson_structure/<datajson_id>', 
+    '/structure/<datajson_id>', 
     methods=['GET'])(get_datajson_structure)
