a3274e36382913a333adbd5c66089db33c561256
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-09 14:53:44 +0400

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,8 +1,8 @@
 # utils/__init__.py
 __all__ = [
-    'args_to_dict'
+    'args_to_dict',
+    '_',
+    'get_translation_dict'
 ]
 
-from .common import args_to_dict
-
-
+from .common import args_to_dict, _, get_translation_dict
