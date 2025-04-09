43adbe8f1a7737a426c733b2a963b0137726d9cf
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-09 15:02:15 +0400

    Update __init__.py

--- app/base/api/translate/__init__.py
+++ app/base/api/translate/__init__.py
@@ -1,9 +1,9 @@
-# api/translation/__init__.py
+# api/translate/__init__.py
 from flask import Blueprint
 
-from .views import translate
+from .views import translate_g, translate_s
 
 translate_bp = Blueprint('translate', __name__, url_prefix='/translate')
 
-translate_bp.route('/<input_text>', methods=['GET'])(translate)
-translate_bp.route('/spec/<spec_dict_names>/<input_text>', methods=['GET'])(translate)
+translate_bp.route('/<input_text>', methods=['GET'])(translate_g)
+translate_bp.route('/spec/<input_text>', methods=['GET'])(translate_s)
