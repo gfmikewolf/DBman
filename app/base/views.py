ba9f1878fabb472f2cd486bb8e2e123547233a32
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-09 14:53:16 +0400

    Update views.py

--- app/base/views.py
+++ app/base/views.py
@@ -1,5 +1,5 @@
 # app/base/views.py
-from flask import render_template
+from flask import redirect, request, render_template, current_app, session
 from app.utils.templates import PageNavigation
 
 # 本蓝图的基础导航
@@ -9,3 +9,9 @@ navigation = PageNavigation ({
 
 def index():
     return render_template('index.jinja', navigation=navigation.index)
+
+def set_lang(lang: str):
+    if lang not in current_app.config['LANGSET']:
+        raise ValueError(f'Lang: {lang} not in valid language set')
+    session['LANG'] = lang
+    return redirect(request.referrer)
