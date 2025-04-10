e2edcceb60bc4b96163729926e2142d8185f6798
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-10 11:05:12 +0400

    Update types.py

--- app/database/contract/types.py
+++ app/database/contract/types.py
@@ -9,15 +9,14 @@ class ClauseAction(Enum):
     REMOVE = 'remove'
     UPDATE = 'update'
 
-
 class ClauseType(Enum):
     """
     { 'clause_entity', 'clause_scope', 'clause_expiry' }
     """
+    BASIC = 'clause'
     ENTITY = 'clause_entity'
     SCOPE = 'clause_scope'
     EXPIRY = 'clause_expiry'
-
 
 class ClausePos(Enum):
     """
@@ -26,3 +25,8 @@ class ClausePos(Enum):
     MAINBODY = 'mainbody'
     ANNEX = 'annex'
     APPENDIX = 'appendix'  
+
+class ExpiryType(Enum):
+    FIXED_EXPIRY_DATE = 'FD'
+    LINKED_TO_CONTRACT = 'LC'
+    LAST_CHILD_EXPIRY_DATE_OR_FIXED_DATE = 'LLOF'
