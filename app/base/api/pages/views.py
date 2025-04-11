b394bb39778365beb2fc31008040305466298eac
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-11 05:42:56 +0400

    Create views.py

---
+++ app/base/api/pages/views.py
@@ -0,0 +1,11 @@
+from flask import current_app
+from app.base.crud.utils import fetch_modify_form_viewer, fetch_instance
+from app.extensions import db_session
+
+def get_spec_form_entries(table_name: str) -> str:
+    with db_session() as db_sess:
+        instance = fetch_instance(table_name, '_new', db_sess)
+        data = fetch_modify_form_viewer(instance, db_sess, True)
+    template = current_app.jinja_env.get_template('macros/form-entries-dbman.jinja')
+    
+    return template.module.FormEntriesDBMan(data = data) # type: ignore
