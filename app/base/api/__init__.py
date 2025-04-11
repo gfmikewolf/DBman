6ed8a9c5607aeccae404cc0a8f8461d8baca015d
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-11 05:47:33 +0400

    Update __init__.py

--- app/base/api/__init__.py
+++ app/base/api/__init__.py
@@ -2,10 +2,12 @@
 
 from flask import Blueprint
 
-from .datajson import datajson_bp
+# from .datajson import datajson_bp
 from .translate import translate_bp
+from .pages import pages_bp
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-api_bp.register_blueprint(datajson_bp)
+# api_bp.register_blueprint(datajson_bp)
 api_bp.register_blueprint(translate_bp)
+api_bp.register_blueprint(pages_bp)
