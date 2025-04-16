42eebe7171bf5fe02ce860639b442c205771b237
gfmikewolf <xiaolongmikeliu@hotmail.com>
2025-04-16 16:52:47 +0400

    Update views.py

--- app/base/auth/views.py
+++ app/base/auth/views.py
@@ -2,20 +2,19 @@
 from flask import abort, redirect, render_template, request, session
 from app.extensions import db_session, Base
 
-
-def app_login() -> str | None:
+def app_login():
     if request.method == 'GET':
         return render_template('login.jinja')
     
     if request.method != 'POST':
-        redirect('/')
+        return redirect('/')
 
     # request.method == 'POST'
     User = Base.model_map['user']
     user_name = request.form.get('user_name', None)
     user_pw = request.form.get('password', None)
     if not user_name or not user_pw:
-        abort(403)
+        return render_template('login.jinja', failed=True)
 
     with db_session() as sess:
         db_user = sess.query(User).filter_by(user_name=user_name).first()
@@ -24,5 +23,8 @@ def app_login() -> str | None:
             merged_role = sum(db_user.user_roles[1:], db_user.user_roles[0]) # type: ignore
     if merged_role:
         session['_priv'] = merged_role.table_privilege # type: ignore
-        session['user_name'] = db_user.user_name # type: ignore       
-    redirect('/')
+        session['user_name'] = db_user.user_name # type: ignore
+        return redirect('/')
+    else:
+        return render_template('login.jinja', failed=True)    
+    
